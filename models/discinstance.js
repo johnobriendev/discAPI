f363572e4e3e56ee25421b49dfeee63ec4e063fa
John O'Brien <johnobrien.dev@gmail.com>
2024-06-25 17:17:24 -0400

    update plastics in discinstance model

--- models/discinstance.js
+++ models/discinstance.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const DiscinstanceSchema = new Schema({
   disc: {type: Schema.Types.ObjectId, ref: "Disc", required: true},
-  plastic: {type: String, required: true, enum: ["DX", "Pro", "Champion", "Star", "GStar", "Halo Star"]},
+  plastic: {type: String, required: true, enum: ["DX", "Pro", "KC Pro", "JK Pro", "Champion", "Star", "GStar", "Halo Star"]},
   weight: {type: Number, required: true, minLength: 130, maxLength: 180},
   color: {type: String, required: true,},
   price: { type: Number, required: true }, 
