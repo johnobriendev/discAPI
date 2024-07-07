d1345dc0d98d299a059ee6d0884c408c1a176c0c
John O'Brien <johnobrien.dev@gmail.com>
2024-07-06 20:03:41 -0400

    create passport config and user model

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const passport = require('passport');
 const compression = require("compression");
 const helmet = require("helmet");
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 require('dotenv').config();
 const catalogRouter = require("./routes/catalog");
 
@@ -34,6 +35,8 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+require('./config/passport');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -55,7 +58,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
